ee8be671fa34a8bdf96b1b7057e4a32a093e8031
jefferson <jefferson.nascimento@fastsolucoes.com.br>
2021-09-23 19:42:48 -0300

    [FORM-2] Esqueleto do FormTemplate-Driven

---
+++ ProjectForm/src/app/FormTemplate/template-driven/template-driven.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template-driven',
+  templateUrl: './template-driven.component.html',
+  styleUrls: ['./template-driven.component.css']
+})
+export class TemplateDrivenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    let dados = `
+    Nome: ${form.value.nome}
+    Telefone: ${form.value.telefone}
+    Endereço: ${form.value.endereco}
+    `;
+
+    console.log(dados);
+  }
+}
