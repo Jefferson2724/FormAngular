3989fdd7cc285ec485c2a1f581b9e75b8ce688c7
jefferson <jefferson.nascimento@fastsolucoes.com.br>
2021-09-24 19:01:10 -0300

    [FORM-6] Fazendo validacoes para campos do formulario

--- ProjectForm/src/app/FormTemplate/template-driven/template-driven.component.ts
+++ ProjectForm/src/app/FormTemplate/template-driven/template-driven.component.ts
@@ -8,23 +8,63 @@ import { DataUserModels } from 'src/app/models/dataUser-models';
   styleUrls: ['./template-driven.component.css']
 })
 export class TemplateDrivenComponent implements OnInit {
-
+  mensagem: String;
+  
   dataUser: DataUserModels = {
     Nome:"",
     Telefone: null,
     Endereco:"",
     Cidade:"",
     Estado:"",
-    CEP:""
-  }
+    CEP:null,
+  };
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   onSubmit(form: NgForm) {
+    this.resetMessageError();
+
+    if(this.checkFields(form)){
+      console.log(this.mensagem);
+      return;
+    }
+
     
     console.log(this.dataUser);
+  }
+
+  private checkFields(form: NgForm) {
+    if(form.value['name'] == ""){
+      this.mensagem = this.mensagem.replace('%s', "nome");
+      return true;
+    }
+    if(form.value['phone'] == null){
+      this.mensagem = this.mensagem.replace('%s', 'telefone');
+      return true;
+    }
+    if(form.value['address'] == ""){
+      this.mensagem = this.mensagem.replace('%s', "address");
+      return true;
+    }
+    if(form.value['city'] == ""){
+      this.mensagem = this.mensagem.replace('%s', "city");
+      return true;
+    }
+    if(form.value['state'] == ""){
+      this.mensagem = this.mensagem.replace('%s', "state");
+      return true;
+    }
+    if(form.value['CEP'] == null){
+      this.mensagem = this.mensagem.replace('%s', "CEP");
+      return true;
+    }
+
+    return false;
+  }
+
+  resetMessageError(){
+    this.mensagem = "O campo %s não esta preenchido";
   }
 }
