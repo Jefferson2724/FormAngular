66be73efd153c47e353b2d962515104598f78c8d
jefferson <jefferson.nascimento@fastsolucoes.com.br>
2021-09-25 13:20:04 -0300

    [FORM-10] Criando prototipo do service

--- ProjectForm/src/app/services/form-driven.service.ts
+++ ProjectForm/src/app/services/form-driven.service.ts
@@ -1,11 +1,49 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { DataUserModels } from '../models/dataUser-models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormDrivenService {
+  private readonly url = `${environment.apiURL}`;
 
   constructor(private http: HttpClient) { }
   
+  registerUser(body:DataUserModels){
+    let responseRegister: BehaviorSubject<DataUserModels> = new BehaviorSubject(undefined);
+
+    this.http.post<DataUserModels>(`${this.url}/register/user`, body, {observe: 'response'})
+    .subscribe(
+      response => {
+        if(!response.status){
+          console.log("Deu errado...");
+          responseRegister.next(response.body);
+        }else {
+          console.log("Deu bom");
+          responseRegister.next(response.body);
+        }
+      },
+      error => {
+        console.log(error.status);
+
+        responseRegister.next(undefined);
+      }
+    );
+
+    return responseRegister.asObservable();
+  }
+
+  //Está função sera implementada mais tarde !
+  listUser(){
+    this.http.get<DataUserModels[]>(`${ this.url }/listUser`, {observe: 'response'}).subscribe( 
+    response => {
+      console.log(response);
+
+      return response.body;
+    });
+  }
 }
+
